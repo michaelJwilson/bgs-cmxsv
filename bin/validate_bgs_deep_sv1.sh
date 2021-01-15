7671916b94756aeb7867ae027b7fe1863b512636
michaelJwilson <mjwilson@lbl.gov>
2021-01-14 16:23:26 -0800

    Cleanup deep truth cats.  Add summary.

--- bin/validate_bgs_deep_sv1.sh
+++ bin/validate_bgs_deep_sv1.sh
@@ -11,6 +11,7 @@ from   astropy.table import Table, vstack, join
 from   desitarget.sv1.sv1_targetmask import desi_mask as sv1_desi_mask
 from   desitarget.sv1.sv1_targetmask import bgs_mask as sv1_bgs_mask
 from   pkg_resources                 import resource_filename
+from   badz                          import is_badz
 
 # [BGS_FAINT,           0, "BGS faint targets",              {obsconditions: BRIGHT|GRAY|DARK}]
 # [BGS_BRIGHT,          1, "BGS bright targets",             {obsconditions: BRIGHT}]
@@ -20,37 +21,42 @@ from   pkg_resources                 import resource_filename
 
 # https://github.com/desihub/desitarget/blob/master/doc/nb/target-selection-bits-and-bitmasks.ipynb
 
-write = True
+write = False
 
+# Get SV1 exposures. 
 epath = resource_filename('bgs-cmxsv', 'dat/sv1-exposures.fits')
-exps  = Table.read(epath)
-exps  = exps[exps['TARGETS'] == 'BGS+MWS']
+aexps = Table.read(epath)
+
+# Limit to BGS. 
+exps  = aexps[aexps['TARGETS'] == 'BGS+MWS']
 
 tiles = np.unique(exps['TILEID'].data)
 
-# TILEID  NIGHT  EXPID
+# Limit to exposures that made it into deep.  
 dexps = resource_filename('bgs-cmxsv', 'dat/blanc_deep_explist.dat') 
 dexps = np.loadtxt(dexps)
 dexps = exps[np.isin(exps['EXPID'], dexps[:,2])]
 
+# Calculate summed depth for deep. 
 dexps          = dexps['TILEID', 'TILERA', 'TILEDEC', 'EXPTIME', 'B_DEPTH', 'R_DEPTH', 'Z_DEPTH']
 
 tinfo          = astropy.table.unique(dexps['TILEID', 'TILERA', 'TILEDEC'], keys='TILEID')
 
-dexps_grouped = dexps.group_by('TILEID')
-dexps_binned  = dexps_grouped.groups.aggregate(np.sum)
-dexps         = dexps_binned
+dexps_grouped  = dexps.group_by('TILEID')
+dexps_binned   = dexps_grouped.groups.aggregate(np.sum)
+dexps          = dexps_binned
 
-dexps         = dexps['TILEID', 'EXPTIME', 'B_DEPTH', 'R_DEPTH', 'Z_DEPTH']
-dexps         = join(dexps, tinfo, keys='TILEID', join_type='left')
+dexps          = dexps['TILEID', 'EXPTIME', 'B_DEPTH', 'R_DEPTH', 'Z_DEPTH']
+dexps          = join(dexps, tinfo, keys='TILEID', join_type='left')
 
 dexps.sort('EXPTIME')
 
+# Collect targets assigned, on a working fiber, with a good z. 
 dexps['NBGSA'] = 0
 dexps['NBGSW'] = 0
 dexps['NBGSZ'] = 0
 
-deep_cache = {}
+deep_cache     = {}
 
 for tileid in tiles:
     deep_cache[tileid] = Table()
@@ -67,40 +73,38 @@ for tileid in tiles:
             
             zbest    = Table(zbest)
             fmap     = Table(fmap)
-
+            
             print('\n\n{} \t {} \t ({} \t {})'.format(tileid, petal, len(zbest), len(fmap)))
             
-            tinfo    = fmap['TARGETID', 'TARGET_RA', 'TARGET_DEC', 'FLUX_R', 'FIBERFLUX_R', 'PHOTSYS', 'SV1_DESI_TARGET', 'SV1_BGS_TARGET', 'DESI_TARGET', 'BGS_TARGET'] 
+            tinfo    = fmap['TARGETID', 'TARGET_RA', 'TARGET_DEC', 'FLUX_G', 'FLUX_R', 'FLUX_Z', 'FIBERFLUX_G', 'FIBERFLUX_R', 'FIBERFLUX_Z', 'PHOTSYS', 'SV1_DESI_TARGET', 'SV1_BGS_TARGET', 'DESI_TARGET', 'BGS_TARGET'] 
             tinfo    = astropy.table.unique(tinfo, keys='TARGETID')
             
-            # cols = fmap.dtype.names
-            # cols = [x for x in cols if 'DESI_TARGET' in x]
-            # print(tileid, cols)
+            # fmap[TILEID, FIBERSTATUS, EXPID]
+            fmap     = join(fmap['TARGETID', 'EXPID', 'FIBERSTATUS'], aexps['EXPID', 'B_DEPTH', 'R_DEPTH', 'Z_DEPTH'], keys='EXPID', join_type='left')
+            fmap     = fmap[fmap['FIBERSTATUS'] == 0]
 
-            deep     = join(zbest, tinfo, keys='TARGETID', join_type='left')
+            fmap_grouped = fmap.group_by('TARGETID')
+            fmap         = fmap_grouped.groups.aggregate(np.sum)            
+ 
+            deep         = join(zbest, tinfo, keys='TARGETID', join_type='left')
+            deep         = join(deep,   fmap, keys='TARGETID', join_type='left')
+
+            del deep['EXPID']
+            del deep['FIBERSTATUS']
             
-            # FIBERSTATUS cut.                                                                                                                                                                                  
-            # fibstat  = fmap['TARGETID', 'FIBERSTATUS']
-            # fibstat  = fibstat.group_by('TARGETID')
-            # fibstat  = fibstat.groups.aggregate(np.all)
-
-            # badfiber = fibstat['FIBERSTATUS']
-
-            # deep     = join(deep, fibstat, keys='TARGETID', join_type='left')
-
             assert  len(deep) == 500
 
-            # Limit to BGS.                                                                                                                                                                                                                   
+            # Limit to BGS.                                                                                                                                                                                                                  
             badbgs   = (deep['SV1_DESI_TARGET'] & sv1_desi_mask['BGS_ANY']) == 0
 
-            nbright  = (deep['SV1_BGS_TARGET'] & sv1_bgs_mask['BGS_BRIGHT']) == 0
-            nfaint   = (deep['SV1_BGS_TARGET'] & sv1_bgs_mask['BGS_FAINT'])  == 0
+            nbright  = (deep['SV1_BGS_TARGET']  & sv1_bgs_mask['BGS_BRIGHT']) == 0
+            nfaint   = (deep['SV1_BGS_TARGET']  & sv1_bgs_mask['BGS_FAINT'])  == 0
             
             # Limit to faint | bright bgs only.
-            badbgs   = badbgs | (nbright & nfaint)
+            # badbgs = badbgs | (nbright & nfaint)
             
             # Limit to bright bgs only.                                                                                                                                                                                               
-            badbgs   = badbgs | nbright
+            # badbgs = badbgs | nbright
             
             #
             deep     = deep[~badbgs]
@@ -117,14 +121,14 @@ for tileid in tiles:
             badfiber = deep['NODATA']
             deep     = deep[~badfiber]
 
+            del deep['NODATA']
+            
             # Assigned to a working fiber.
             dexps['NBGSW'][dexps['TILEID'] == tileid] += len(deep)
                         
-            # Good redshifts (in deep)
-            badz     = (deep['ZWARN'] > 0) | (deep['DELTACHI2'] < 40.) | (deep['SPECTYPE'] == 'STAR')
-            badz     = badz | (deep['Z'] < 0.0) | (deep['Z'] > 0.6)
-            badz     = badz | (deep['ZERR'] > (0.0005 * (1. + deep['Z'])))
-            
+            badz     = is_badz(deep, dX2_lim=100.)
+
+            # Good redshifts (in deep)  
             deep     = deep[~badz]
             
             for label, lost in zip(['FIBER', 'BGS', 'Z'], [badfiber, badbgs, badz]):
@@ -134,18 +138,24 @@ for tileid in tiles:
             deep_cache[tileid] = vstack((deep_cache[tileid], deep))
 
             dexps['NBGSZ'][dexps['TILEID'] == tileid] += len(deep)
-                                                 
+            
     if len(deep_cache[tileid]) == 0:
         del deep_cache[tileid]
 
 if write:
+    print('\n\n')
+    
     for tileid in deep_cache.keys():
+        print('Writing BGS {} deep zbest.'.format(tileid))
+        
         deep_cache[tileid].sort('TARGETID')
         deep_cache[tileid].write('/global/cscratch1/sd/mjwilson/desi/SV1/spectra/truth/bgs_deep_truth_{:d}.fits'.format(tileid), overwrite=True)
 
 #
 dexps['BGSSUCCESS_%'] = ['{:.2f}'.format(100. * x['NBGSZ'] / x['NBGSW']) for x in dexps]
-        
+
+dexps.write('/global/cscratch1/sd/mjwilson/desi/SV1/spectra/truth/summary.fits', overwrite=True)
+
 print('\n\n')
 print(dexps)
 print('\n\nDone.\n\n')
--- py/bgs-cmxsv/badz.py
+++ py/bgs-cmxsv/badz.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 
-def is_badz(cat, verbose=False):
+def is_badz(cat, dX2_lim=40., verbose=False):
     badz     = np.zeros(len(cat)).astype(bool) 
 
     labels   = ['ZWARN', 'DELTACHI2', 'SPECTYPE', 'ZRANGE', 'ZERR']
-    cuts     = [cat['ZWARN'] > 0, cat['DELTACHI2'] < 40., cat['SPECTYPE'] == 'STAR', (cat['Z'] < 0.0) | (cat['Z'] > 0.6), cat['ZERR'] > (0.0005 * (1. + cat['Z']))]
+    cuts     = [cat['ZWARN'] > 0, cat['DELTACHI2'] < dX2_lim, cat['SPECTYPE'] == 'STAR', (cat['Z'] < 0.0) | (cat['Z'] > 0.6), cat['ZERR'] > (0.0005 * (1. + cat['Z']))]
 
     for label, cut in zip(labels, cuts):
         badz = badz | cut
